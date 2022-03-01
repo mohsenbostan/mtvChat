c57ae591ab7abf58216afbaa2c01e84a30c68a49
Kevin Yeh <kyeh4@jh.edu>
2022-03-01 14:58:27 -0800

    credentials

--- v2/utils.js
+++ v2/utils.js
@@ -19,5 +19,5 @@ function escapeHtml(message) {
 }
 
 function TwitchAPI(url) {
-    return $.getJSON(url + (url.search(/\?/) > -1 ? '&' : '?') + 'client_id=' + client_id);
+    return $.getJSON(url + (url.search(/\?/) > -1 ? '&' : '?') + 'client_id=p31p8buadkrrel3fdzq7268cq6q7li');
 }
